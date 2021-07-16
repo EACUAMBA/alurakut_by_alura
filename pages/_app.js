69eeb524759727e96823f259066fa4adf05129d5
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-07-16 14:04:08 +0200

    Aprendi á como criar uma projecto react e como usar o sistema dos componentes de estilo par agrupar e organizar meus CSS em pastas e usalos em qualquer view.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/**
+ * Usamos este arquivo para para aplicar configurações padrões para todo o projecto. 
+ * ex: uma cor padrão para o body.
+ */
 
+//Algumas configurações que já foram feitas para o body.
 const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
 `
 
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,49 @@ import styled from 'styled-components'
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const MainGrid = styled.main` //Criando a minha propria configuração para tag main, quando eu for a usar a tag MainGrid que criei tudo o que estiver nela será emcapsulado em uma tag main.
+display: grid;
+grid-gap: 10px; //Espaço entre as boxes
+padding: 16px;
+
+  @media(min-width: 860px){
+    grid-template-areas: "profileArea feedArea comunidadeArea";
+    grid-template-columns: 160px 1fr 312px; // 1fr - paga ua fração do espaço que existe.
+  }
+  
+`;
+
+const Box = styled.div`
+  background-color: #ffffff;
+  border-radius: 8px; 
+  padding: 10px;
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div style={{ gridArea: 'perfilArea' }}>
+        <Box>
+          Perfil
+        </Box>
+      </div>
+
+      <div style={{ gridArea: 'feedArea2' }}>
+        <Box>
+          Feed
+        </Box>
+      </div>
+
+      <div style={{ gridArea: 'comunidadeArea3' }}>
+        <Box>
+          Comunidade
+        </Box>
+      </div>
+    </MainGrid>
+  );
+
+
 }
